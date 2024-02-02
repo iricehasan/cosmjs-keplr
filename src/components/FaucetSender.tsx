3c121ebec66bd73bb0799e69203f1194931ea46f
iricehasan <hsn.irice@gmail.com>
2024-02-02 16:45:32 +0300

    Testnet Chain Info

--- src/components/FaucetSender.tsx
+++ src/components/FaucetSender.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, Component, MouseEvent } from "react"
 import { Coin, SigningStargateClient, StargateClient } from "@cosmjs/stargate"
 import styles from '../styles/Home.module.css'
-import { Window as KeplrWindow } from "@keplr-wallet/types";
+import { ChainInfo, Window as KeplrWindow } from "@keplr-wallet/types"
 
 declare global {
     interface Window extends KeplrWindow {}
@@ -65,6 +65,74 @@ export class FaucetSender extends Component<FaucetSenderProps, FaucetSenderState
             return
         }
     }
+
+    getTestnetChainInfo = (): ChainInfo => ({
+        chainId: "theta-testnet-001",
+        chainName: "theta-testnet-001",
+        rpc: "https://rpc.sentry-01.theta-testnet.polypore.xyz/",
+        rest: "https://rest.sentry-01.theta-testnet.polypore.xyz/",
+        bip44: {
+            coinType: 118,
+        },
+        bech32Config: {
+            bech32PrefixAccAddr: "cosmos",
+            bech32PrefixAccPub: "cosmos" + "pub",
+            bech32PrefixValAddr: "cosmos" + "valoper",
+            bech32PrefixValPub: "cosmos" + "valoperpub",
+            bech32PrefixConsAddr: "cosmos" + "valcons",
+            bech32PrefixConsPub: "cosmos" + "valconspub",
+        },
+        currencies: [
+            {
+                coinDenom: "ATOM",
+                coinMinimalDenom: "uatom",
+                coinDecimals: 6,
+                coinGeckoId: "cosmos",
+            },
+            {
+                coinDenom: "THETA",
+                coinMinimalDenom: "theta",
+                coinDecimals: 0,
+            },
+            {
+                coinDenom: "LAMBDA",
+                coinMinimalDenom: "lambda",
+                coinDecimals: 0,
+            },
+            {
+                coinDenom: "RHO",
+                coinMinimalDenom: "rho",
+                coinDecimals: 0,
+            },
+            {
+                coinDenom: "EPSILON",
+                coinMinimalDenom: "epsilon",
+                coinDecimals: 0,
+            },
+        ],
+        feeCurrencies: [
+            {
+                coinDenom: "ATOM",
+                coinMinimalDenom: "uatom",
+                coinDecimals: 6,
+                coinGeckoId: "cosmos",
+                gasPriceStep: {
+                    low: 1,
+                    average: 1,
+                    high: 1,
+                },
+            },
+        ],
+        stakeCurrency: {
+            coinDenom: "ATOM",
+            coinMinimalDenom: "uatom",
+            coinDecimals: 6,
+            coinGeckoId: "cosmos",
+        },
+        coinType: 118,
+        features: ["stargate", "ibc-transfer", "no-legacy-stdTx"],
+    })
+    
 
     // The render function that draws the component at init and at state change
     render() {
